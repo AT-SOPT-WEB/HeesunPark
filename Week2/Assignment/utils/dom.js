8fa6d7b4ebd701e299a7d34d73229e4ee29e52c6
heesunee <heesun729@uos.ac.kr>
2025-04-25 20:03:46 +0900

    design: 아이콘 변경

--- Week2/Assignment/utils/dom.js
+++ Week2/Assignment/utils/dom.js
@@ -16,7 +16,7 @@ export const createTableRow = (todo) => {
 
   const tdCompleted = document.createElement('td');
   tdCompleted.className = 'todo-table__completed';
-  tdCompleted.textContent = todo.completed ? 'O' : 'X';
+  tdCompleted.textContent = todo.completed ? '⭕' : '❌';
 
   const tdTitle = document.createElement('td');
   tdTitle.className = 'todo-table__title';
