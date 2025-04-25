280264bf6bc980c07058697cd5b555310429e769
heesunee <heesun729@uos.ac.kr>
2025-04-25 15:36:22 +0900

    feat: 체크박스 기능 추가

--- Week2/Assignment/app.js
+++ Week2/Assignment/app.js
@@ -82,3 +82,65 @@ const deleteRow = () => {
 
 const btnDelete = document.querySelector('.btn__actions--delete');
 btnDelete.addEventListener('click', deleteRow);
+
+const handleFinish = () => {
+  const ids = getCheckedTd();
+
+  if (ids.length === 0) {
+    alert('완료 처리할 항목을 선택하세요.');
+    return;
+  }
+
+  const hasCompleted = todoList.some(
+    (todo) => ids.includes(todo.id) && todo.completed
+  );
+
+  if (hasCompleted) {
+    alert('이미 완료된 항목이 있습니다.');
+
+    return;
+  }
+
+  todoList.forEach((todo) => {
+    if (ids.includes(todo.id)) {
+      todo.completed = true;
+    }
+  });
+  setTodos(storageKey, todoList);
+  renderList(todoList);
+};
+
+const btnFinish = document.querySelector('.btn__actions--finish');
+btnFinish.addEventListener('click', handleFinish);
+
+const thCheckBox = document.querySelector('.todo-table__checkbox');
+
+const getRowCheckboxes = () =>
+  document.querySelectorAll('.todo-table__body input[type="checkbox"]');
+
+const handlethCheckBoxClick = (event) => {
+  const checked = event.target.checked;
+  const checkboxes = getRowCheckboxes();
+
+  thCheckBox.indeterminate = false;
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = checked;
+  });
+};
+
+const handleRowCheckBoxClick = () => {
+  const checkboxes = getRowCheckboxes();
+  const allChecked = Array.from(checkboxes).every((cb) => cb.checked);
+  const someChecked = Array.from(checkboxes).some((cb) => cb.checked);
+
+  thCheckBox.checked = allChecked;
+};
+
+thCheckBox.addEventListener('click', handlethCheckBoxClick);
+
+document.addEventListener('change', (e) => {
+  if (e.target.matches('.todo-table__body input[type="checkbox"]')) {
+    handleRowCheckBoxClick();
+  }
+});
