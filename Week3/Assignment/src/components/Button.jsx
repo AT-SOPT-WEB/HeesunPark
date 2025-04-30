c1a5bdaf4d01c3992f74a97ee52b7a58be94cc7d
heesunee <heesun729@uos.ac.kr>
2025-05-01 02:30:15 +0900

    feat: 버튼 공통 컴포넌트

--- Week3/Assignment/src/components/Button.jsx
+++ Week3/Assignment/src/components/Button.jsx
@@ -1,1 +1,13 @@
-rafce;
+const Button = ({ selected, children, onClick }) => {
+  return (
+    <button
+      type="button"
+      className={`rounded-xl px-2 py-1 text-body text-white transition-colors duration-300 ease-in-out ${selected ? "bg-secondary" : "bg-primary"} `}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- Week3/Assignment/src/components/Header.jsx
+++ Week3/Assignment/src/components/Header.jsx
@@ -1,7 +1,25 @@
+import Button from "./Button";
+import { useState } from "react";
 const Header = () => {
+  const [selected, setSelected] = useState("github");
+
   return (
-    <div className="flex h-24 flex-col items-center justify-center bg-primary">
+    <div className="flex h-24 flex-col items-center justify-center gap-2 bg-primary">
       <h1 className="text-title-1 text-white">⚾ 숫자야구 & 깃허브 검색 🔍</h1>
+      <div className="flex gap-2">
+        <Button
+          selected={selected === "github"}
+          onClick={() => setSelected("github")}
+        >
+          깃허브 검색 🔍
+        </Button>
+        <Button
+          selected={selected === "baseball"}
+          onClick={() => setSelected("baseball")}
+        >
+          숫자야구 ⚾
+        </Button>
+      </div>
     </div>
   );
 };
