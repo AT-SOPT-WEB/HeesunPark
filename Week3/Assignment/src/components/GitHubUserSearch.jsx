1b3ba427eedfb4d1d2bffc704223e72cb8716601
heesunee <heesun729@uos.ac.kr>
2025-05-02 01:42:13 +0900

    feat: 최근 검색어 로직 분리

--- Week3/Assignment/src/components/GitHubUserSearch.jsx
+++ Week3/Assignment/src/components/GitHubUserSearch.jsx
@@ -1,10 +1,9 @@
 import Input from "./Input";
-import Card from "./Card";
 import { useGitHubUser } from "../hooks/useGitHubUser";
 import { useEffect, useState } from "react";
 import { GITHUB_KEY } from "../constant/storageKey";
 import { setItem } from "../utils/storageUtils";
-import List from "./List";
+import UserHistoryList from "./UserHistoryList";
 import { useUserHistory } from "../hooks/useUserHistory";
 import { renderUserInfo } from "../hooks/renderUserInfo";
 const GitHubUserSearch = () => {
@@ -48,32 +47,15 @@ const GitHubUserSearch = () => {
         onKeyDown={handleKeyDown}
         placeholder="GitHub 프로필을 검색해보세요"
       />
-      <ul className="flex gap-2">
-        {[...userList].reverse().map((user, i) => (
-          <List
-            key={`${user}-${i}`}
-            onClick={() => {
-              getUserInfo(user);
-              setUserInput(user);
-              setShowCard(true);
-            }}
-            className="flex cursor-pointer items-center gap-1 hover:text-blue-500"
-          >
-            {user}
-            <button
-              type="button"
-              onClick={(e) => {
-                // 부모 onClick 방지
-                e.stopPropagation();
-                handleDeleteStore(user);
-              }}
-              className="flex cursor-pointer items-center"
-            >
-              <i className="fa-solid fa-xmark"></i>
-            </button>
-          </List>
-        ))}
-      </ul>
+      <UserHistoryList
+        userList={userList}
+        onUserClick={(user) => {
+          getUserInfo(user);
+          setUserInput(user);
+          setShowCard(true);
+        }}
+        onDeleteUser={handleDeleteStore}
+      />
       {content}
     </div>
   );
---
+++ Week3/Assignment/src/components/UserHistoryList.jsx
@@ -0,0 +1,28 @@
+import List from "./List";
+const UserHistoryList = ({ userList, onUserClick, onDeleteUser }) => {
+  return (
+    <ul className="flex gap-2">
+      {[...userList].reverse().map((user, i) => (
+        <List
+          key={`${user}-${i}`}
+          onClick={() => onUserClick(user)}
+          className="flex cursor-pointer items-center gap-1 hover:text-blue-500"
+        >
+          {user}
+          <button
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              onDeleteUser(user);
+            }}
+            className="flex cursor-pointer items-center"
+          >
+            <i className="fa-solid fa-xmark"></i>
+          </button>
+        </List>
+      ))}
+    </ul>
+  );
+};
+
+export default UserHistoryList;
