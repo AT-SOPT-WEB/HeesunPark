979f8f48adaec8f692d6de4cf810c3b96ac22bfa
heesunee <heesun729@uos.ac.kr>
2025-05-09 18:23:29 +0900

    init: router 설정 추가

--- Week4/Assignment/src/App.tsx
+++ Week4/Assignment/src/App.tsx
@@ -1,5 +1,8 @@
-function App() {
-  return <></>;
-}
+import router from '@router/Router';
+import { RouterProvider } from 'react-router';
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
